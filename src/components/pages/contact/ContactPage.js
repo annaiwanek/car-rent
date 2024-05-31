639d1fb549dcf915eecee1ecd5a4b51ede46a2b9
Anna Iwanek-Sikorska <aniaiwa4@gmail.com>
2024-05-31 20:30:27 +0200

    rozbudowa strony kontakt

--- src/components/pages/contact/ContactPage.js
+++ src/components/pages/contact/ContactPage.js
@@ -19,13 +19,61 @@ function ContactPage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Dodaj logikę wysyłania formularza
+    // Dodanie logikę wysyłania formularza
     console.log('Form Data:', formData);
   };
 
   return (
     <Container className="contact-page">
-      <h1 className="my-5 text-center">Kontakt</h1>
+      <h1 className="my-3">Kontakt</h1> {/* Zmniejszenie marginesu górnego */}
+      
+      {/* Sekcja informacji kontaktowych */}
+      <Row className="contact-info mb-3">
+        <Col xs={12} md={6}>
+          <h3>Centrala</h3>
+          <p>
+            <strong>Dane firmy</strong><br />
+            CAR RENT Polska Sp. z o.o.<br />
+            00-123 Warszawa, ul. Przykładowa 1<br />
+            NIP: 1234567890, REGON: 9876543210, KRS: 0123456789<br />
+            Sąd Rejonowy w Warszawie<br />
+            Kapitał zakładowy: 100 000 PLN opłacony w całości.
+          </p>
+          <p className="address-section">
+            <strong>Adres do korespondencji</strong><br />
+            ul. Przykładowa 1, 00-123 Warszawa<br />
+            e-mail: kontakt@carrent.pl
+          </p>
+        </Col>
+        <Col xs={12} md={6}>
+          <h3>Dział Obsługi Klienta</h3>
+          <p>
+            telefon: +48 123 456 789<br />
+            e-mail: kontakt@carrent.pl
+          </p>
+          <h3>Rozliczenia wynajmów i zwrot depozytu</h3>
+          <p>
+            telefon: +48 123 456 789<br />
+            e-mail: ksiegowosc@carrent.pl
+          </p>
+          <h3>Rezerwacje średnioterminowe</h3>
+          <p>
+            telefon: +48 987 654 321<br />
+            e-mail: handlowy@carrent.pl
+          </p>
+          <h3>Dział reklamacji</h3>
+          <p>
+            e-mail: reklamacje@carrent.pl
+          </p>
+          <h3>Dział marketingu</h3>
+          <p>
+            e-mail: marketing@carrent.pl
+          </p>
+        </Col>
+      </Row>
+
+      <h2 className="mt-5 mb-4 text-center">Wyślij wiadomość</h2> {/* Dodanie większego marginesu górnego */}
+
       <Form onSubmit={handleSubmit}>
         <Row>
           <Col xs={12} md={6} className="mb-3">
