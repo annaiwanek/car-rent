836936c8c1f4a8c71324637c8ae15d8c7c5dc72f
Anna Iwanek-Sikorska <aniaiwa4@gmail.com>
2024-06-01 12:32:07 +0200

    stylowanie formularza na HOME

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import CarouselHome from '../../carousel/CarouselHome';
 import './Home.css'; 
 import OfferSection from './OfferSection'; 
 import HowItWorks from './HowItWorks'; 
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        navigate('/reservation');
+    };
+
     return (
         <Container fluid>
             {/* Sekcja ofertowa */}
@@ -14,10 +22,10 @@ function Home() {
             {/* Formularz wyszukiwania samochodu */}
             <Row className="justify-content-center mt-4">
                 <Col md={10}>
-                    <Form className="d-flex flex-wrap justify-content-between align-items-center">
+                    <Form className="search-form d-flex flex-wrap justify-content-between align-items-center" onSubmit={handleSearch}>
                         <Form.Group controlId="formCity" className="mr-2">
                             <Form.Label>Miasto odbioru</Form.Label>
-                            <Form.Control as="select">
+                            <Form.Control as="select" className="custom-select">
                                 <option>Warszawa</option>
                                 <option>Kraków</option>
                                 <option>Wrocław</option>
@@ -28,12 +36,12 @@ function Home() {
 
                         <Form.Group controlId="formPickupDate" className="mr-2">
                             <Form.Label>Data odbioru</Form.Label>
-                            <Form.Control type="date" />
+                            <Form.Control type="date" className="custom-input" />
                         </Form.Group>
 
                         <Form.Group controlId="formReturnDate" className="mr-2">
                             <Form.Label>Data zwrotu</Form.Label>
-                            <Form.Control type="date" />
+                            <Form.Control type="date" className="custom-input" />
                         </Form.Group>
 
                         <Button variant="primary" type="submit" className="search-car">
@@ -52,7 +60,7 @@ function Home() {
 
             {/* Sekcja "Jak to działa" */}
             <HowItWorks />
-          </Container>
+        </Container>
     );
 }
 
