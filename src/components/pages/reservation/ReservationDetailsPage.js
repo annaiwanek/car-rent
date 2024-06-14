41cf3a0912514ecc9f6fd6573a23231b9a8f3648
Anna Iwanek-Sikorska <aniaiwa4@gmail.com>
2024-06-14 19:21:50 +0200

    poprawki szczegoly rezerwacji

--- src/components/pages/reservation/ReservationDetailsPage.js
+++ src/components/pages/reservation/ReservationDetailsPage.js
@@ -51,79 +51,81 @@ function ReservationDetailsPage() {
           </Card>
         </Col>
       </Row>
-      <Card className="additional-services">
-        <h5>Wybierz pakiet ochronny</h5>
-        <Row>
-          <Col md={3} className={`service-option ${selectedOption === 'standard' ? 'selected' : ''}`}>
-            <Form.Check
-              type="radio"
-              name="insurance"
-              value="standard"
-              label={<strong>Pakiet zniesienia odpowiedzialności STANDARD W CENIE</strong>}
-              checked={selectedOption === 'standard'}
-              onChange={handleOptionChange}
-            />
-            <p>✔ Podstawowe ubezpieczenie OC, AC i NW gwarantujące bezpieczne użytkowanie pojazdu.</p>
-            <p className="service-price">Cena: W CENIE</p>
-          </Col>
-          <Col md={3} className={`service-option ${selectedOption === 'gold' ? 'selected' : ''}`}>
-            <Form.Check
-              type="radio"
-              name="insurance"
-              value="gold"
-              label={<strong>Pakiet zniesienia odpowiedzialności GOLD +39 PLN</strong>}
-              checked={selectedOption === 'gold'}
-              onChange={handleOptionChange}
-            />
-            <p>✔ Opcja ubezpieczenie w wersji Gold gwarantuje zniesienie udziału własnego w szkodach AC do 0 zł.</p>
-            <p className="service-price">Cena: +39 PLN</p>
-          </Col>
-          <Col md={3} className={`service-option ${selectedOption === 'gold-eu' ? 'selected' : ''}`}>
-            <Form.Check
-              type="radio"
-              name="insurance"
-              value="gold-eu"
-              label={<strong>Pakiet zniesienia odpowiedzialności GOLD EU +89 PLN</strong>}
-              checked={selectedOption === 'gold-eu'}
-              onChange={handleOptionChange}
-            />
-            <p>✔ Opcja ubezpieczenie w wersji Gold EU gwarantuje zniesienie udziału własnego w szkodach AC do 0 zł.</p>
-            <p className="service-price">Cena: +89 PLN</p>
-          </Col>
-          <Col md={3} className={`service-option ${selectedOption === 'abroad' ? 'selected' : ''}`}>
-            <Form.Check
-              type="radio"
-              name="insurance"
-              value="abroad"
-              label={<strong>Wyjazd za granicę w strefie 1 +149 PLN</strong>}
-              checked={selectedOption === 'abroad'}
-              onChange={handleOptionChange}
-            />
-            <p>✔ Opcja wyjazdu za granicę w strefie 1.</p>
-            <p className="service-price">Cena: +149 PLN</p>
-          </Col>
-        </Row>
-      </Card>
-      <Card className="additional-services">
-        <h5>Wybierz dodatkowe opcje</h5>
-        <Button
-          className="toggle-additional-options"
-          onClick={() => setShowAdditionalOptions(!showAdditionalOptions)}
-        >
-          Dodatkowe opcje <span>{showAdditionalOptions ? '▲' : '▼'}</span>
-        </Button>
-        {showAdditionalOptions && (
-          <Form className="additional-options-form">
-            <div className="form-check-wrapper">
+      <div className="services-section">
+        <Card className="additional-services">
+          <h5>Wybierz pakiet ochronny</h5>
+          <Row>
+            <Col md={3} className={`service-option ${selectedOption === 'standard' ? 'selected' : ''}`}>
               <Form.Check
-                type="checkbox"
-                label="Końcowe mycie pojazdu za 39 PLN"
-                className="form-check-item"
+                type="radio"
+                name="insurance"
+                value="standard"
+                label={<strong>Pakiet zniesienia odpowiedzialności STANDARD W CENIE</strong>}
+                checked={selectedOption === 'standard'}
+                onChange={handleOptionChange}
               />
-            </div>
-          </Form>
-        )}
-      </Card>
+              <p>✔ Podstawowe ubezpieczenie OC, AC i NW gwarantujące bezpieczne użytkowanie pojazdu.</p>
+              <p className="service-price">Cena: W CENIE</p>
+            </Col>
+            <Col md={3} className={`service-option ${selectedOption === 'gold' ? 'selected' : ''}`}>
+              <Form.Check
+                type="radio"
+                name="insurance"
+                value="gold"
+                label={<strong>Pakiet zniesienia odpowiedzialności GOLD +39 PLN</strong>}
+                checked={selectedOption === 'gold'}
+                onChange={handleOptionChange}
+              />
+              <p>✔ Opcja ubezpieczenie w wersji Gold gwarantuje zniesienie udziału własnego w szkodach AC do 0 zł.</p>
+              <p className="service-price">Cena: +39 PLN</p>
+            </Col>
+            <Col md={3} className={`service-option ${selectedOption === 'gold-eu' ? 'selected' : ''}`}>
+              <Form.Check
+                type="radio"
+                name="insurance"
+                value="gold-eu"
+                label={<strong>Pakiet zniesienia odpowiedzialności GOLD EU +89 PLN</strong>}
+                checked={selectedOption === 'gold-eu'}
+                onChange={handleOptionChange}
+              />
+              <p>✔ Opcja ubezpieczenie w wersji Gold EU gwarantuje zniesienie udziału własnego w szkodach AC do 0 zł.</p>
+              <p className="service-price">Cena: +89 PLN</p>
+            </Col>
+            <Col md={3} className={`service-option ${selectedOption === 'abroad' ? 'selected' : ''}`}>
+              <Form.Check
+                type="radio"
+                name="insurance"
+                value="abroad"
+                label={<strong>Wyjazd za granicę w strefie 1 +149 PLN</strong>}
+                checked={selectedOption === 'abroad'}
+                onChange={handleOptionChange}
+              />
+              <p>✔ Opcja wyjazdu za granicę w strefie 1.</p>
+              <p className="service-price">Cena: +149 PLN</p>
+            </Col>
+          </Row>
+        </Card>
+        <Card className="additional-services">
+          <h5>Wybierz dodatkowe opcje</h5>
+          <Button
+            className="toggle-additional-options"
+            onClick={() => setShowAdditionalOptions(!showAdditionalOptions)}
+          >
+            Dodatkowe opcje <span>{showAdditionalOptions ? '▲' : '▼'}</span>
+          </Button>
+          {showAdditionalOptions && (
+            <Form className="additional-options-form">
+              <div className="form-check-wrapper">
+                <Form.Check
+                  type="checkbox"
+                  label="Końcowe mycie pojazdu za 39 PLN"
+                  className="form-check-item"
+                />
+              </div>
+            </Form>
+          )}
+        </Card>
+      </div>
       <Button variant="primary" className="proceed-button">
         Dalej
       </Button>
